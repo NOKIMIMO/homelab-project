61766a145444fa313e2d426651a73799bbb9b547
Steven Chouk <steven.chouk@gmail.com>
2026-04-29 11:38:50 +0200

    Refacto_back

--- homelab-backend/src/main/kotlin/com/homelab/core/model/DiskData.kt
+++
@@ -1,7 +0,0 @@
-package com.homelab.core.model
-data class DiskData(
-        val total: Double,
-        val used: Double,
-        val coreStorageUsed: Double,
-        val modulesStorageUsed: Double
-)
--- homelab-backend/src/main/kotlin/com/homelab/core/model/RamData.kt
+++
@@ -1,7 +0,0 @@
-package com.homelab.core.model
-data class RamData(
-        val total: Double,
-        val used: Double,
-        val coreUsed: Double,
-        val modulesUsed: Double
-)
