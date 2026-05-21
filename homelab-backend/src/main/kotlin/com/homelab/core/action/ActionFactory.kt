18de0da3f53a3a248ec49587db14e73f97160e47
Nicolas Janikowski <njanikow@catmktg.com>
2026-05-21 17:53:12 +0200

    started on plugin

---
+++ homelab-backend/src/main/kotlin/com/homelab/core/action/ActionFactory.kt
@@ -0,0 +1,27 @@
+package com.homelab.core.action
+
+import com.homelab.core.model.action.*
+import com.homelab.core.plugin.PluginRegistry
+import org.springframework.stereotype.Service
+
+@Service
+class ActionFactory(private val pluginRegistry: PluginRegistry) {
+    private val builtins: Map<String, Action>
+
+    init {
+        builtins = mapOf(
+            ActionsEnum.UPLOAD_FILE.name to UploadFileAction(),
+            ActionsEnum.GET_FILE.name to GetFileAction(),
+            ActionsEnum.DELETE.name to DeleteAction(),
+            ActionsEnum.LIST.name to ListAction(),
+            ActionsEnum.READ.name to ReadAction()
+        )
+    }
+
+    fun resolve(typeName: String): Action? {
+        val byBuiltin = builtins[typeName]
+        if (byBuiltin != null) return byBuiltin
+        return pluginRegistry.getAction(typeName)
+    }
+}
+
