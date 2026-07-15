c5f4fcae4629194b6cd8e7325276ffa8182ef11c
Karl OELSCHLAGER <oelschlager.karl@gmail.com>
2026-07-15 10:45:48 +0200

    feat: spring upgrade

--- homelab-backend/build.gradle.kts
+++ homelab-backend/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.1.21"
     kotlin("plugin.spring") version "2.1.21"
     id("org.jetbrains.kotlin.plugin.jpa") version "2.1.21"
-    id("org.springframework.boot") version "3.3.5"
+    id("org.springframework.boot") version "3.5.16"
     id("io.spring.dependency-management") version "1.1.6"
 }
 
